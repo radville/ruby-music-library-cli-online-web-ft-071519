c91a9ca36375f69f48b062cfbf91f5859392986a
Laura Radville <laura.radville@gmail.com>
2019-08-02 19:33:45 +0000

    Automatically backed up by Learn

--- lib/music_controller.rb
+++ lib/music_controller.rb
@@ -26,6 +26,10 @@ class MusicLibraryController
           list_artists
         when "list genres"
           list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
       end
     end
     
