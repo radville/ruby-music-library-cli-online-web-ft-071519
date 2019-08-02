a9c63eb903bd217509111de772099ce3cf552707
Laura Radville <laura.radville@gmail.com>
2019-08-02 18:44:50 +0000

    Done.

--- lib/music_controller.rb
+++ lib/music_controller.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class MusicLibraryController
 
   def initialize(path = './db/mp3s')
@@ -18,22 +20,49 @@ class MusicLibraryController
       puts "What would you like to do?"
       input = gets.strip
     end
+  end
     
-    def list_songs
-      sorted = Song.all.sort_by { |s| s.name}
-      sorted.each_with_index { |song, i| puts "#{i+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
+  def list_songs
+    sorted = Song.all.sort_by { |s| s.name}
+    sorted.each_with_index { |song, i| puts "#{i+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
+  end
+    
+  def list_artists
+    sorted = Artist.all.sort_by { |a| a.name}
+    sorted.each_with_index { |artist, i| puts "#{i+1}. #{artist.name}" }
+  end
+    
+  def list_genres
+    sorted = Genre.all.sort_by { |g| g.name}
+    sorted.each_with_index { |genre, i| puts "#{i+1}. #{genre.name}" }
+  end
+    
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    artist = Artist.find_by_name(input)
+    if artist 
+      sorted_songs = artist.songs.sort_by { |s| s.name }
+      sorted_songs.each_with_index do |song, i|
+        puts "#{i+1}. #{song.name} - #{song.genre.name}"
+      end
     end
-    
-    def list_artists
-      sorted = Artist.all.sort_by { |a| a.name}
-      sorted.each_with_index { |artist, i| puts "#{i+1}. #{artist.name}" }
+  end
+  
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+    genre = Genre.find_by_name(input)
+    if genre 
+      sorted_songs = genre.songs.sort_by { |s| s.name }
+      sorted_songs.each_with_index do |song, i|
+        puts "#{i+1}. #{song.artist.name} - #{song.name}"
+      end
     end
-    
-    def list_genres
-      sorted = Genre.all.sort_by { |g| g.name}
-      sorted.each_with_index { |genre, i| puts "#{i+1}. #{genre.name}" }
-    end
-    
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
   end
   
 end
