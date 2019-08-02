2963a191408c9d7f381f0accee035f0fa5614b1f
Laura Radville <laura.radville@gmail.com>
2019-08-02 18:49:34 +0000

    Automatically backed up by Learn

--- lib/music_controller.rb
+++ lib/music_controller.rb
@@ -64,9 +64,11 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.chomp
+    if input == Integer
       sorted = Song.all.sort_by { |s| s.name}
       song = sorted[input.to_i+1]
       puts "Playing #{song.name} by #{song.artist.name}"
+    end
   end
   
 end
