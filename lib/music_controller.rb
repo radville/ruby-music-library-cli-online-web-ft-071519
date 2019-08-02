882b62b08a0c4defcbd314d6693ed219607a9505
Laura Radville <laura.radville@gmail.com>
2019-08-02 19:06:18 +0000

    Automatically backed up by Learn

--- lib/music_controller.rb
+++ lib/music_controller.rb
@@ -20,6 +20,9 @@ class MusicLibraryController
       puts "What would you like to do?"
       input = gets.strip
     end
+    if input == "list songs"
+      list_songs
+    end
   end
     
   def list_songs
